fe133c55399f4364191edb1703ddd61d1e97678b
julioxbs@gmail.com <julioxbs@gmail.com>
2022-02-25 19:06:26 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <header class="position-relative">
+      <div class="header-text text-center p-5">
+        <h1>Simple, traffic-based pricing</h1>
+
+        <p>
+          Sign-up for out 30-day trial. No credit card required.
+        </p>
+      </div>
+  </header>
+
+  <Pricing />
+</template>
+
+<script>
+import Pricing from './components/Pricing.vue'
+export default {
+  components: {
+    Pricing,
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@300;600;800&display=swap');
+:root {
+  /* (Full Slider Bar) */
+  --clr-SoftCyan: hsl(174, 77%, 80%);
+  /* (Slider Backround) */
+  --clr-StrongCyan: hsl(174, 86%, 45%);
+  /* (Discount Background) */
+  --clr-LightGrayishRed: hsl(14, 92%, 95%);
+  /* (Discount Text) */
+  --clr-LightRed: hsl(15, 100%, 70%);
+  /* (CTA Text) */
+  --clr-PaleBlue: hsl(226, 100%, 87%);
+
+  /* (Pricing Component Background) */
+  --clr-white: hsl(0, 0%, 100%);
+  /* (Main Background) */
+  --clr-veryPaleBlue: hsl(230, 100%, 99%);
+  /* (Empty Slider Bar) */
+  --clr-lightGrayishBlue: hsl(224, 65%, 95%);
+  /* (Toggle Background) */
+  --clr-lightBgBlue: hsl(223, 50%, 87%);
+  /* (Text) */
+  --clr-grayishBlue: hsl(225, 20%, 60%);
+  /* (Text & CTA Background) */
+  --clr-darkDesaturatedBlue: hsl(227, 35%, 25%);
+}
+
+body {
+  font-family: 'Manrope', sans-serif;
+}
+
+ul {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+header {
+  height: 25rem;
+  width: 100%;
+  background-image: url('/images/bg-pattern.svg');
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  /* background-color: var(--clr-lightGrayishBlue);
+  border-bottom-left-radius: 10rem; */
+}
+
+header h1 {
+  font-size: 21px;
+  font-weight: bold;
+  color: var(--clr-darkDesaturatedBlue);
+}
+
+header p {
+  font-size: 15px;
+  color: var(--clr-grayishBlue);
+}
+
+.header-text {
+  background-image: url('/images/pattern-circles.svg');
+  background-repeat: no-repeat;
+  background-position: center;
+
+  max-width: 400px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Pricing.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="box-price rounded shadow">
+      <div class="box-header p-3 text-center">
+          <div class="price d-md-flex align-items-center justify-content-between">
+              <p class="fw-bold text-muted views">100K PAGEVIEWS</p>
+              <p class="text-muted"><span class="fw-bold">${{rangeValue}}.00</span> / month</p>
+          </div>
+
+          <div class="inputValue">
+            <input v-model="rangeValue" type="range" class="w-100" min="0" max="32">
+          </div>
+
+          <div class="info d-flex align-items-center justify-content-center gap-3 text-muted mt-5">
+              <p>Monthly Billing</p>
+
+              <p @click="btnActive" class="activ" :class="monthBilling === true ? 'active-bg' : ''">
+                  <span class="circle" :style="!monthBilling ? 'left: 4px;' : 'right: 4px;'"></span>
+              </p>
+              
+              <p>Yearly Billing 
+                  <span class="discont fw-bold rounded" style="font-size: 12px; padding: 1px 2px;">-25%</span>
+              </p>
+          </div>
+      </div>
+
+      <hr>
+
+      <div class="box-foot p-3 text-center d-md-flex align-items-center justify-content-between">
+          <ul class="p-0 m-0 d-flex flex-column gap-2">
+              <li v-for="{name, id} in lists" :key="id" class="text-muted" style="font-size: 15px fw-bold">
+                  <span><img src="/images/icon-check.svg" alt="icon"></span>
+                  {{name}}
+              </li>
+          </ul>
+
+          <a href="#" class="button fw-bold">Start my trial</a>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            rangeValue: 16,
+            monthBilling: false,
+
+            lists: [
+                {name: 'Unlimited websites', id: 1},
+                {name: '100% data ownership', id: 2},
+                {name: 'Email reports', id: 3},
+            ]
+        }
+    },
+
+    methods: {
+        btnActive() {
+            this.monthBilling = !this.monthBilling
+        }
+    }
+}
+</script>
+
+<style scoped>
+.box-price {
+    width: 100%;
+    max-width: 500px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -20%);
+    background: #fff;
+}
+
+input[type=range]{
+    -webkit-appearance: none;
+}
+
+input[type=range]::-moz-range-progress {
+    background: var(--clr-StrongCyan);
+}
+
+input[type=range]::-webkit-slider-runnable-track {
+    width: 100%;
+    height: 10px;
+    background: var(--clr-SoftCyan);
+    border: none;
+    border-radius: 3px;
+}
+
+input[type=range]::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
+    background: var(--clr-StrongCyan);
+    background-image: url('/images/icon-slider.svg');
+    background-repeat: no-repeat;
+    background-position: center;
+    cursor: pointer;
+    margin-top: -15px;
+    box-shadow: 1px 1px 1px var(--clr-SoftCyan), 0px 0px 10px var(--clr-SoftCyan); 
+}
+
+input[type=range]::-moz-range-thumb {
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
+    background: var(--clr-StrongCyan);
+    background-image: url('/images/icon-slider.svg');
+    background-repeat: no-repeat;
+    background-position: center;
+    cursor: pointer;
+    margin-top: -15px;
+    box-shadow: 1px 1px 1px var(--clr-SoftCyan), 0px 0px 10px var(--clr-SoftCyan); 
+}
+
+.box-price .views {
+    font-size: 14px;
+}
+
+.box-price .price span {
+    font-size: 35px;
+    color: var(--clr-darkDesaturatedBlue);
+}
+
+.box-price .info {
+    font-size: 15px;
+}
+
+.activ {
+    background: var(--clr-lightGrayishBlue);
+    border-radius: 15px;
+    width: 60px;
+    height: 25px;
+    position: relative;
+    transition: background .5s;
+}
+
+.circle {
+    background: #fff;
+    border-radius: 50%;
+    display: inline-block;
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    top: 2px;
+    transition: all .5s;
+}
+
+.active-bg {
+    background: var(--clr-StrongCyan);
+}
+
+.button {
+    background: var(--clr-darkDesaturatedBlue);
+    padding: 8px 12px;
+    display: inline-block;
+    border-radius: 15px;
+    margin-top: 10px;
+    font-size: 13px;
+    color: var(--clr-PaleBlue);
+}
+
+.discont {
+    color: var(--clr-LightRed);
+    background-color: var(--clr-LightGrayishRed);
+}
+</style>
